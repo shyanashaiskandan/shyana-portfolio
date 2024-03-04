559305a83452348f9976ab088b6b55f161e13e5a
Shyana <shyana.shais@gmail.com>
2024-03-03 22:15:03 -0500

    making navbar sticky

--- src/app/components/Navbar.tsx
+++ src/app/components/Navbar.tsx
@@ -1,34 +1,57 @@
-import React, { useState } from 'react'
+'use client'
+import React, { useEffect, useState } from 'react'
 import Link from "next/link"
 
 
-const Navbar = () => {
-  return (
-    <div className= 'header flex bg-mypink'>
-      <div className ='name'>
-        <h2>Shyana Shaiskandan</h2>
-      </div>
-      <div className = 'menu'></div>
 
-        <nav>
-          <ul>
-            <li>
-              <Link href = ''>About</Link>
-            </li>
-            <li>
-              <Link href = ''>Experience</Link>
-            </li>
-            <li>
-              <Link href = ''>Projects</Link>
-            </li>
-            <li>
-              <Link href = ''>Certifications</Link>
-            </li>  
-            <li>
-              <Link href = ''>Contact Me</Link>
-            </li>
-          </ul>
-        </nav>
+const Navbar = () => {
+  const [header, setHeader] = useState(false); 
+
+  const scrollHeader = () => {
+
+    if (window.scrollY >= 20) {
+      setHeader(true)
+    }
+    else {
+      setHeader(false)
+    }
+  }
+
+  useEffect(()=> {
+    window.addEventListener('scroll', scrollHeader)
+
+    return ()=> {
+      window.addEventListener('scroll', scrollHeader)
+    }
+  }, [])
+  return (
+    <div className = "header fixed w-[100%] bg-mypink text-[white]">
+      <div className= 'header flex w-[80%] justify-between m-auto py- [15px]'>
+        <div className ='name'>
+          <h2>Shyana Shaiskandan</h2>
+        </div>
+        <div className = 'menu'></div>
+
+          <nav>
+            <ul className = 'flex gap-[20px]'>
+              <li>
+                <Link href = ''>About</Link>
+              </li>
+              <li>
+                <Link href = ''>Experience</Link>
+              </li>
+              <li>
+                <Link href = ''>Projects</Link>
+              </li>
+              <li>
+                <Link href = ''>Certifications</Link>
+              </li>  
+              <li>
+                <Link href = ''>Contact Me</Link>
+              </li>
+            </ul>
+          </nav>
+      </div>
     </div>
   )
 }
