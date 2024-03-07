7667530bc284fc606d692fddad074b8a97b66e60
Shyana <shyana.shais@gmail.com>
2024-03-07 12:13:22 -0500

    timeline template

--- src/app/experience/page.tsx
+++ src/app/experience/page.tsx
@@ -2,9 +2,50 @@ import React from 'react'
 
 const experience = () => {
   return (
-    <div>
-      <h2 className="py-20">experience</h2>
+    <>
+<div className="content">
+  <h1><center>Experience</center></h1>
+</div>
+<div className="timeline">
+  <div className="container left">
+      <i className="fa fa-code-fork" aria-hidden="true"></i>
+    <div className="content">
+      <h2>Git Basics</h2>
+      <p>Visit <a href="https://learngitbranching.js.org/">this site</a> to learn about Git and become comfortable with basic Git practices</p>
     </div>
+  </div>
+  <div className="container right">
+    <div className="content">
+      <h2>Puppet Language Basics</h2>
+      <p><a href="https://puppet.com/learning-training/kits/puppet-language-basics/">This self-paced course</a> will teach you the basics of understanding and writing Puppet code in less than thirty minutes.</p>
+    </div>
+  </div>
+    <div className="container left">
+    <div className="content">
+      <h2>Puppet Practice Labs</h2>
+      <p>Now you are ready to write some Puppet code in an <a href="https://learn.puppet.com/practicelabcatalog">interactive, browser based environment</a>.</p>
+    </div>
+  </div>
+    <div className="container right">
+    <div className="content">
+      <h2>PE101: Deploy & Discover</h2>
+      <p>At this point you are ready to take the in-person training and implement Puppet in your own environment.<p><i>Coming soon!</i></p></p>
+    </div>
+  </div>
+  <div className="container left">
+    <div className="content">
+      <h2>PE201: Design & Manage</h2>
+      <p>Already have Puppet implemented? Learn best practices for designing and managing your environment.</p><p><i>Coming soon!</i></p>
+    </div>
+  </div>
+    <div className="container right">
+    <div className="content">
+      <h2>Keep going!</h2>
+      <p>The journey never ends. <a href="http://learn.puppet.com">Here are some resources</a> to keep you on your learning path.</p>
+    </div>
+  </div>
+</div>
+</>
   )
 }
 
---
+++ src/app/experience/timelineElements.tsx
@@ -0,0 +1,11 @@
+let timelineElements = [
+{
+    id: 1,
+    title: "Quality Assurance Analyst",
+    company: "Bank of Montreal",
+    date: "January 2022 - April 2022"
+}
+
+];
+
+export default timelineElements; 
