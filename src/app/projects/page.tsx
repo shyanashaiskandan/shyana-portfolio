938d28003613ba1dd8ef97e750758fe5e67c973b
Shyana <shyana.shais@gmail.com>
2024-03-08 12:03:20 -0500

    basic template - projects

--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import Image from "next/image"
 import Link from "next/link"
-import temp from "./governmentlogo.png"
+import temp from "./testing.png"
 import githublogo from "./githublogo.png"
 
 const works = [
@@ -10,6 +10,12 @@ const works = [
   description: "djksfldkjf",
   image: "./temp",
   github: "https://www.youtube.com",
+},
+{
+  name: "Portfolio Website",
+  description: "djksfldkjf",
+  image:"./temp",
+  github:"https://www.youtube.com",
 }
 
 ]
@@ -20,25 +26,25 @@ const projects = () => {
     <div>
       <h1 className="p-10 font-quicksand text-pink text-5xl font-bold"><center>Projects</center></h1>
     </div>
-    <div className = "flex flex-col space-y-28"> 
+    <div className = "pt-10 grid grid-cols-2"> 
         {works.map((work,idx) => {
           return (
           <div key={idx}>
-            <div className = "flex flex-col md:flex-row md:space-x-12">
-              <div className = "md:w-1/2">
+            <div className = "mx-20 flex justify-center md:flex-row md:space-x-12">
+              <div className = "">
                 <Image 
                   src={temp}
                   alt=""
-                  width = {1000}>
+                  width = {400}>
                   </Image>
               </div>
-              <div className= "md:w-1/2">
-                <h1 className = "text-4xl font-bold mb-6">{work.name}</h1>
-                <p className="text-xl leading-7 mb-4 text-neutral-600">{work.description}</p>
+              <div className= "pl-5 m-auto">
+                <h1 className = "text-3xl mb-2 font-bold">{work.name}</h1>
+                <p className="text-l leading-7 mb-4 text-neutral-600">{work.description}</p>
                 <div>
                   <Link href={work.github} target = "_blank">
                     <div className = "hover:-translate-y-1 transition-transform cursor-pointer">
-                    <Image src ={githublogo} alt="" width = {30}></Image>
+                    <Image src ={githublogo} alt="" width = {25}></Image>
                     </div>
                   </Link>
                   </div>
