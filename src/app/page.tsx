c06458c4ff670d5a782f00a54b19b797b84bbd82
Shyana <shyana.shais@gmail.com>
2024-03-28 15:19:55 -0400

    added animation

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,12 +8,14 @@ import headshot from "./headshot.png"
 export default function Home() {
   return (
     <main>
-      <div className = "flex flex-row text-center items-center justify-center mt-24 ">
-      <div className = "w-1/2 pr-10">
-        <h1 className = "font-quicksand text-pink text-6xl font-bold pb-10">Hi, I&#39;m Shyana!</h1>
-        <p className = "text-xl bg-orange text-white">djflsjflds jflsdkjfl dsjflsjdfsld jfssdfjl ssdfkjldskjfl skd jflsjf</p>
+      <div className = "flex flex-row text-center items-center justify-center mt-24 animate-mybounce">
+      <div className = "w-1/2 pr-12">
+        <h1 className = "font-quicksand text-pink text-6xl font-bold pb-12">Hi, I&#39;m Shyana!</h1>
+        <p className = "text-xl bg-orange text-white p-10 ">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
       </div>
-      <Image src={headshot} alt="" width={500} height={200}/>
+      <div className = "pl-10">
+          <Image src={headshot} alt="" width={500} height={200}/>
+      </div>
 
       </div>
 
