bea5e260725180f9835b980ec5dc26fa56b67dc9
SHYANA SHAISKANDAN <sshaiska@uwaterloo.ca>
2024-09-14 15:39:32 -0400

    added projects

--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -1,69 +1,67 @@
 import React from 'react'
 import Image from "next/image"
 import Link from "next/link"
-import temp from "./testing.png"
+import photobooth from "./photo-booth-photo.jpg"
+import portfolio from "./portfolio-photo.png" // Import actual image files
 import githublogo from "./githublogo.png"
+import geoquiz from "./geo-quiz-photo.png"
 
 const works = [
-{
-  name: "Portfolio Website",
-  description: "djksfldkjf",
-  image: "./temp",
-  github: "https://www.youtube.com",
-},
-{
-  name: "Portfolio Website",
-  description: "djksfldkjf",
-  image:"./temp",
-  github:"https://www.youtube.com",
-},
-{
-  name: "Portfolio Website",
-  description: "djksfldkjf",
-  image:"./temp",
-  github:"https://www.youtube.com",
-},
-
+  {
+    name: "Photo Booth Simulator",
+    description: "Raspberry Pi, Python, Flask",
+    image: photobooth, // Use the imported image directly
+    github: "https://www.youtube.com",
+  },
+  {
+    name: "Geography Quiz",
+    description: "MongoDB, Express.JS, React, Node.JS, Bootstrap CSS",
+    image: geoquiz, // Reuse or import another image
+    github: "https://www.youtube.com",
+  },
+  {
+    name: "Portfolio Website",
+    description: "React, Next.JS, Tailwind CSS",
+    image: portfolio, // Another imported image
+    github: "https://www.youtube.com",
+  },
 
 ]
 
-const projects = () => {
+const Projects = () => {
   return (
     <>
-    <div>
-      <h1 className="p-10 font-quicksand text-pink text-5xl font-bold"><center>Projects</center></h1>
-    </div>
-    <div className = "grid grid-cols-2"> 
-        {works.map((work,idx) => {
+      <div>
+        <h1 className="p-10 font-quicksand text-pink text-6xl font-bold">
+          <center>Projects</center>
+        </h1>
+      </div>
+      <div className="grid grid-cols-2">
+        {works.map((work, idx) => {
           return (
-          <div key={idx}>
-            <div className = "m-10 flex justify-center md:flex-row md:space-x-12 animate-mybounce">
-              <div className = "">
-                <Image 
-                  src={temp}
-                  alt=""
-                  width = {400}>
-                  </Image>
-              </div>
-              <div className= "pl-5 m-auto">
-                <h1 className = "text-3xl text-orange mb-2 font-bold">{work.name}</h1>
-                <p className="text-l leading-7 mb-4 italic">{work.description}</p>
-                <div>
-                  <Link href={work.github} target = "_blank">
-                    <div className = "hover:-translate-y-1 transition-transform cursor-pointer">
-                    <Image src ={githublogo} alt="" width = {25}></Image>
-                    </div>
-                  </Link>
+            <div key={idx}>
+              <div className="m-10 flex justify-center md:flex-row md:space-x-12 animate-mybounce">
+                <div className="">
+                  <Image src={work.image} alt="" width={350} className = "border-solid border-4 border-teal-600 hover:border-double hover:border-7" />
+                </div>
+                <div className="pl-5 m-auto">
+                  <h1 className="text-2xl text-orange mb-2 font-bold">{work.name}</h1>
+                  <p className="text-l leading-7 mb-4 italic">{work.description}</p>
+                  <div>
+                    <Link href={work.github} target="_blank">
+                      <div className="hover:-translate-y-1 transition-transform cursor-pointer">
+                        <Image src={githublogo} alt="" width={25} />
+                      </div>
+                    </Link>
                   </div>
+                </div>
               </div>
-
-              </div>
-          </div>
-          )
-          })}
-    </div>
+            </div>
+          );
+        })}
+      </div>
     </>
-  )
-}
+  );
+};
 
-export default projects
+export default Projects;
